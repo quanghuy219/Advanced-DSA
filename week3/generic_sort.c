0dc12d49550364b2a2366dad42374f2a67b11fce
Huy Pham <p.q.huy97@gmail.com>
2017-09-10 21:17:16 +0700

    sort phonebook using generic function

--- week3/generic_sort.c
+++ week3/generic_sort.c
@@ -31,36 +31,37 @@ void exch(void* buff, int size, int i, int j) {
 	memcpy(tmp, (char*)buff+i*size, size);
 	memcpy((char*)buff+i*size, (char*)buff+j*size, size);
 	memcpy((char*)buff+j*size, tmp, size);
+	free(tmp);
 }
 
 void sort(void *a, int size, int l, int r, int (*compare)(const void*,const void*)) {
-  if(r<=l) return;
-  int i=l-1,j=r;
-  int p=l-1,q=r;
-  while(1)
+  	if(r<=l) return;
+  	int i=l-1,j=r;
+  	int p=l-1,q=r;
+  	while(1)
     {
-      while( compare( (char*)a+(++i)*size , (char*)a+r*size ) < 0 );
-      while( compare( (char*)a+r*size , (char*)a+(--j)*size ) < 0 ) if(j==l) break;
-      if(i>=j) break;
-      exch(a,size,i,j);
-      if(compare( (char*)a+(i)*size , (char*)a+r*size ) == 0) exch(a, size, ++p, i);
-      if(compare( (char*)a+(j)*size , (char*)a+r*size ) == 0) exch(a, size, --q, j);
+	    while( compare( (char*)a+(++i)*size , (char*)a+r*size ) < 0 );
+	    while( compare( (char*)a+r*size , (char*)a+(--j)*size ) < 0 ) if(j==l) break;
+	    if(i>=j) break;
+	    exch(a,size,i,j);
+	    if(compare( (char*)a+(i)*size , (char*)a+r*size ) == 0) exch(a, size, ++p, i);
+	    if(compare( (char*)a+(j)*size , (char*)a+r*size ) == 0) exch(a, size, --q, j);
     }
-  exch(a, size, i, r);
-  j=i-1;
-  i=i+1;
-  for(int k=l;k<=p;k++) exch(a, size, k, j--);
-  for(int k=r-1;k>=q;k--) exch(a, size, k, i++);
-  sort(a, size, l, j, compare);
-  sort(a, size, i, r, compare);
+  	exch(a, size, i, r);
+  	j=i-1;
+  	i=i+1;
+  	for(int k=l;k<=p;k++) exch(a, size, k, j--);
+  	for(int k=r-1;k>=q;k--) exch(a, size, k, i++);
+  	sort(a, size, l, j, compare);
+  	sort(a, size, i, r, compare);
 }
 
 void print(int a[], int size) {
-  for (int i = 0; i < size; i++)
-  {
-    printf("%d    ", a[i]);
-  }
-  printf("\n");
+  	for (int i = 0; i < size; i++)
+  	{
+    	printf("%d    ", a[i]);
+  	}
+  	printf("\n");
 }
 
 int main() {
@@ -69,7 +70,7 @@ int main() {
 	create_array(a,size);
 	print(a,size);
 	printf("\n");
-	sort(a,sizeof(int),0,size,compare);
+	sort(a,sizeof(int),0,size-1,compare);
 	print(a,size);
   	return 0;
 }
